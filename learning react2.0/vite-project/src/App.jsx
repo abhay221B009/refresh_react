13666b73abb624581406e83e9318c63e6bbcb3e3
Abhay'sRepo <abhayrajchauhan.976@gmail.com>
2026-04-19 23:13:18 +0530

    methods of using function in button using onClick attribute

--- learning react2.0/vite-project/src/App.jsx
+++ learning react2.0/vite-project/src/App.jsx
@@ -17,13 +17,22 @@ const Header = memo(({ title }) => {
 const HeaderWithButton = () => {
   const [title, setTitle] = useState("");
 
-  const updateButton = () => {
-    setTitle(`Abhay have learnt react upto ${Math.random() * 100} percent`);
-  };
+  // const updateButton = () => {
+  //   setTitle(`Abhay have learnt react upto ${Math.random() * 100} percent`);
+  // };
+  //if we do not want to write this function separately then we can directly write it in onClick event as shown below
 
   return (
     <>
-      <button onClick={updateButton}>Click to know percent</button>
+      <button
+        onClick={() =>
+          setTitle(
+            `Abhay have learnt react upto ${Math.random() * 100} percent`,
+          )
+        }
+      >
+        Click to know percent
+      </button>
       <Header title={title}></Header>
     </>
   );
