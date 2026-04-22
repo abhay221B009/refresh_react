3c87df06385b2cd97f9a2a48b98515600589a6d4
Abhay'sRepo <abhayrajchauhan.976@gmail.com>
2026-04-22 23:00:35 +0530

    useEffect

--- learning react3.0/vite-project/src/App2.jsx
+++ learning react3.0/vite-project/src/App2.jsx
@@ -1,8 +1,42 @@
 // todays agenda: useEffect, useCallback, useMemo, custom hooks, Prop drilling
 import React from "react";
+import { useState, useEffect } from "react";
 
 const App2 = () => {
-  return <div>App2</div>;
+  const [todos, setTodos] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/todos").then(async (res) => {
+      const json = await res.json();
+      console.log("fetched todos from server: ", JSON.stringify(json));
+      setTodos(json.todos);
+    });
+  }, []);
+  return (
+    <div>
+      {todos && todos.length > 0 ? (
+        todos.map((todo) => (
+          <Todo
+            key={todo._id} // ✅ use _id if MongoDB
+            title={todo.title}
+            description={todo.description}
+          />
+        ))
+      ) : (
+        <p>No todos found</p>
+      )}
+    </div>
+  );
+};
+
+const Todo = ({ title, description }) => {
+  return (
+    <>
+      <div>
+        <h1>{title}</h1>
+        <h2>{description}</h2>
+      </div>
+    </>
+  );
 };
 
 export default App2;
