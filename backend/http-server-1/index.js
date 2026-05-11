bc4eb292ed7504dd3c35e9a70dc4146294478b31
Abhay'sRepo <abhayrajchauhan.976@gmail.com>
2026-05-11 22:22:39 +0530

    revised how to get and post request and res from the server

--- backend/http-server-1/index.js
+++ backend/http-server-1/index.js
@@ -1,10 +1,42 @@
 const express = require("express");
-
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("hello world");
+app.use(express.json()); //middleware to parse json body
+// app.get("/", (req, res) => {
+//   res.send("hello world");
+// });
+
+//we can also send variables in response
+// app.get("/", (req, res) => {
+//   let a = 123456;
+
+//   res.send(`${a} , hello world`);
+// });
+
+//we can also send json response to client
+// app.get("/route-handler", (req, res) => {
+//   res.json({
+//     name: "Abhay",
+//     age: 22,
+//   });
+// });
+
+//we can also send html tags in response
+app.get("/route-handler", (req, res) => {
+  res.send("<b>Hi there</b>");
+});
+
+//post request
+app.post("/", (req, res) => {
+  console.log(req.body);
+
+  console.log(req.headers["authorization"]);
+
+  res.send({
+    msg: "2+2=4",
+    body: req.body,
+  });
 });
 
 app.listen(port, () => {
