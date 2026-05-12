88661abea28137f50b8f2719a74ac3642dc3aca1
Abhay'sRepo <abhayrajchauhan.976@gmail.com>
2026-05-13 01:05:31 +0530

    learned about get post put and delete

---
+++ backend/http-server-1/http-server-2/index.js
@@ -0,0 +1,21 @@
+// const express = require("express");
+// const app = express();
+// const port = 3001;
+
+// const sum = (n) => {
+//   let ans = 0;
+//   for (let i = 0; i <= n; i++) {
+//     ans += i;
+//   }
+//   return ans;
+// };
+
+// app.get("/", (req, res) => {
+//   const n = req.query.n;
+//   const ans = sum(n);
+//   res.send(`the sum of first ${n} natural numbers is ${ans}`);
+// });
+
+// app.listen(port, () => {
+//   console.log(`app listening on port ${port}`);
+// });
--- backend/http-server-1/index.js
+++ backend/http-server-1/index.js
@@ -1,44 +1,44 @@
-const express = require("express");
-const app = express();
-const port = 3000;
+// const express = require("express");
+// const app = express();
+// const port = 3000;
 
-app.use(express.json()); //middleware to parse json body
-// app.get("/", (req, res) => {
-//   res.send("hello world");
-// });
+// app.use(express.json()); //middleware to parse json body
+// // app.get("/", (req, res) => {
+// //   res.send("hello world");
+// // });
 
-//we can also send variables in response
-// app.get("/", (req, res) => {
-//   let a = 123456;
+// //we can also send variables in response
+// // app.get("/", (req, res) => {
+// //   let a = 123456;
 
-//   res.send(`${a} , hello world`);
-// });
+// //   res.send(`${a} , hello world`);
+// // });
 
-//we can also send json response to client
+// //we can also send json response to client
+// // app.get("/route-handler", (req, res) => {
+// //   res.json({
+// //     name: "Abhay",
+// //     age: 22,
+// //   });
+// // });
+
+// //we can also send html tags in response
 // app.get("/route-handler", (req, res) => {
-//   res.json({
-//     name: "Abhay",
-//     age: 22,
+//   res.send("<b>Hi there</b>");
+// });
+
+// //post request
+// app.post("/", (req, res) => {
+//   console.log(req.body);
+
+//   console.log(req.headers["authorization"]);
+
+//   res.send({
+//     msg: "2+2=4",
+//     body: req.body,
 //   });
 // });
 
-//we can also send html tags in response
-app.get("/route-handler", (req, res) => {
-  res.send("<b>Hi there</b>");
-});
-
-//post request
-app.post("/", (req, res) => {
-  console.log(req.body);
-
-  console.log(req.headers["authorization"]);
-
-  res.send({
-    msg: "2+2=4",
-    body: req.body,
-  });
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`);
+// });
